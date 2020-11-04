55b79ae5a14974600be754b5d4873f2603ea620b
Jørgen Verhaeren <jjaverhaeren@gmail.com>
2020-11-04 11:50:43 +0100

    Opdracht deel 3finished

--- Module7_React/React Opdrachten/my-groceries-app/src/components/List.js
+++ Module7_React/React Opdrachten/my-groceries-app/src/components/List.js
@@ -2,14 +2,27 @@ import React, {Component} from "react"
 import ListItem from "./ListItem"
 
 class List extends Component {
+    constructor() {
+        super()
+        this.state = {
+            groceryItems: [
+                { id:1, title: "Paprika"},
+                { id:2, title: "Appels"},
+                { id:3, title: "Brood"},
+                { id:4, title: "Melk"},
+                { id:5, title: "Muesli"},
+            ], 
+        }
+    }
+
+
+    
+
+   
     render() {
         return (
             <ul>
-                <ListItem title="Paprika" />
-                <ListItem title="Appels" />
-                <ListItem title="Brood" />
-                <ListItem title="Melk" />
-                <ListItem title="Muesli" />
+                {this.state.groceryItems.map(item => <ListItem key={item.id} item={item} />)}
             </ul>
         )
     }
--- Module7_React/React Opdrachten/my-groceries-app/src/components/ListItem.js
+++ Module7_React/React Opdrachten/my-groceries-app/src/components/ListItem.js
@@ -2,7 +2,12 @@ import React from "react"
 
 function ListItem(props) {
     return (
-        <p>{props.title}</p>
+        <li 
+        key={props.item.id}
+        className="list-item"
+        // onClick={clickItem}
+        value={props.item.title}
+        >{props.item.title}</li>
     )
 }
 
